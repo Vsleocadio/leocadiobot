b122c8a9ac3213259ae275c40c54b59608cfe727
Leocád1o <4001268@alunouninter.com>
2023-02-08 21:11:43 -0300

    update
    update

--- BotLeocadio.py
+++ BotLeocadio.py
@@ -13,7 +13,7 @@ class Botinstagram:
 
     def __init__(self):
         self.window = Tk()
-        self.window.iconbitmap('iconinsta.ico')
+        self.window.iconbitmap('itens/iconinsta.ico')
         self.window.eval('tk::PlaceWindow . center')
         self.window.title('LeocadioBOT')
         self.window.minsize(width=350, height=150)
@@ -61,7 +61,7 @@ class Botinstagram:
         
             {}
         '''.format(lista_comentarios))
-        service = Service('chromedriver')
+        service = Service('itens/chromedriver')
         service.creationflags = CREATE_NO_WINDOW
         self.navegador = webdriver.Chrome(service=service)
         self.navegador.get('https://www.instagram.com')
@@ -70,17 +70,17 @@ class Botinstagram:
         while True:
             for coments in lista_comentarios:
                 time.sleep(4)
-                textarea = self.navegador.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[3]/div/form/textarea')
+                textarea = self.navegador.find_element_by_tag_name('textarea')
                 time.sleep(1)
                 textarea.click()
                 time.sleep(1)
-                textarea = self.navegador.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[3]/div/form/textarea')
+                textarea = self.navegador.find_element_by_tag_name('textarea')
                 time.sleep(6)
                 textarea.clear()
                 time.sleep(6)
                 textarea.send_keys(coments)
                 time.sleep(6)
-                self.navegador.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[3]/div/form/button/div').click()
+                self.navegador.find_element_by_xpath('/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[3]/div/form/div/div[2]/div').click()
             time.sleep(260)
             self.navegador.refresh()
             time.sleep(5)
